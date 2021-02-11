fe4f121c8a08879ba154e82d839a9d022970f1ca
Zboru <zboru99@gmail.com>
2021-02-11 14:26:13 +0100

    enable source maps in webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').vue()
+mix.sourceMaps()
+    .js('resources/js/app.js', 'public/js').vue()
     .sass('resources/scss/app.scss', 'public/css')
