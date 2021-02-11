b0ad7b5ddf9dd3d59aba7257bed0efb95128797f
Zboru <zboru99@gmail.com>
2021-02-11 14:26:43 +0100

    change password reset view render

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -8,17 +8,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
+use Inertia\Inertia;
 
 class NewPasswordController extends Controller
 {
     /**
      * Display the password reset view.
      *
-     * @return \Illuminate\View\View
+     * @return \Inertia\Response
      */
     public function create(Request $request)
     {
-        return view('auth.reset-password', ['request' => $request]);
+        return Inertia::render('Auth/PasswordReset', [
+            'request' => $request
+        ]);
     }
 
     /**
