d4264a8bdd852c3373bf9b2edddd44cca9ef51a5
Zboru <zboru99@gmail.com>
2021-02-11 11:17:04 +0100

    remove default route boilerplate

--- routes/web.php
+++ routes/web.php
@@ -3,23 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return Inertia::render('Dashboard/Index');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
+// Import authorization related routes
 require __DIR__.'/auth.php';
+
+Route::middleware(['auth'])->group(function() {
+    Route::get('/', function () {
+        return Inertia::render('Dashboard/Index');
+    });
+});
