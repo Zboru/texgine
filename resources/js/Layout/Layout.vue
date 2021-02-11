9813525e034c704a07a19303b0d74ae2b5fc8d7b
Zboru <zboru99@gmail.com>
2021-02-11 15:16:42 +0100

    create early stage of layout

--- resources/js/Layout/Layout.vue
+++ resources/js/Layout/Layout.vue
@@ -1,6 +1,23 @@
 <template>
     <div>
-        <h1>Siema</h1>
+        <nav>
+            <ul>
+                <li>texGine</li>
+                <li>
+                    <inertia-link href="/dashboard">Dashboard</inertia-link>
+                </li>
+                <li>
+                    <inertia-link href="/browse">Browse</inertia-link>
+                </li>
+                <li>
+                    <inertia-link href="/games">My Games</inertia-link>
+                </li>
+                <li class="flex-spacer"></li>
+                <li>
+                    <inertia-link href="/logout" method="post" as="button">Logout</inertia-link>
+                </li>
+            </ul>
+        </nav>
         <slot></slot>
     </div>
 </template>
@@ -11,6 +28,26 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+nav {
+    width: 100%;
+    height: 70px;
+    background: beige;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
+    ul {
+        width: 1180px;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        list-style-type: none;
+
+        li {
+            padding: 20px 30px;
+            border-left: 1px solid gray;
+        }
+    }
+}
 </style>
