c73afd9b0df23fe463a9f1f6548695758d55b539
Zboru <zboru99@gmail.com>
2021-02-11 15:16:24 +0100

    update web.php for temp testing

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,13 @@ use Inertia\Inertia;
 // Import authorization related routes
 require __DIR__.'/auth.php';
 
+
+
 Route::middleware(['auth'])->group(function() {
     Route::get('/', function () {
+        return 1;
+    });
+    Route::get('/dashboard', function () {
         return Inertia::render('Dashboard/Index');
     });
 });
