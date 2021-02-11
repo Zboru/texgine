4bc103b1e11aef35f5a897951eb1293e07c963ee
Zboru <zboru99@gmail.com>
2021-02-11 15:16:32 +0100

    lint

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque eum maiores saepe sit? Ab aperiam architecto, aut consectetur cupiditate dolor doloremque ex harum, magnam placeat quasi quis, ratione soluta ut!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque eum maiores saepe sit? Ab aperiam architecto,
+        aut consectetur cupiditate dolor doloremque ex harum, magnam placeat quasi quis, ratione soluta ut!
     </div>
 </template>
 
 <script>
 export default {
-name: "Index"
+    name: "Index",
 }
 </script>
 
