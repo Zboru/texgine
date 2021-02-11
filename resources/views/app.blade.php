d84555ce8872eceb8ab0eccba894eed810c4470d
Zboru <zboru99@gmail.com>
2021-02-11 10:43:11 +0100

    lint

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <title>texGine</title>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet"></link>
     <script src="{{ mix('/js/app.js') }}" defer></script>
 </head>
 <body>
