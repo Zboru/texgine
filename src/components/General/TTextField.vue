73b906b27326adbf3ab67a80f7b7ed58e3e6032c
Zboru <zboru99@gmail.com>
2021-08-30 14:24:47 +0200

    poprawki do komponentu text-field

--- src/components/General/TTextField.vue
+++ src/components/General/TTextField.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <label :for="inputID" v-if="label">
-      <span class="text-xs text-gray-500">{{ label }}</span>
+      <span class="text-xs select-none text-gray-500">{{ label }}</span>
     </label>
     <div class="relative">
       <div class="absolute bg-white text-gray-300 pointer-events-none w-6 h-6 absolute top-2 right-2">
         <slot name="icon"></slot>
       </div>
-      <input autocomplete="off" @keydown="$emit('keydown', $event)" @focus="$emit('focus')" @blur="$emit('blur')"
+      <input :type="type" autocomplete="off" @keydown="$emit('keydown', $event)" @focus="$emit('focus')" @blur="$emit('blur')"
              :id="inputID"
-             v-model="modelValue" type="text" :placeholder="placeholder"
+             v-model="modelValue" :placeholder="placeholder"
              class="block w-full px-3 py-2 pr-10 text-black placeholder-gray-400
           transition duration-100 ease-in-out bg-white border border-gray-300
            rounded shadow-sm focus:border-gray-500 focus:ring-2 focus:ring-gray-500
@@ -26,6 +26,12 @@ export default {
     label: String,
     placeholder: String,
     value: String,
+    type: {
+      type: String,
+      default() {
+        return 'text';
+      },
+    },
   },
   data() {
     return {
