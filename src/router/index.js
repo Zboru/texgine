567e0dbf9b5cd1db7d814f8aceb19f93640132fd
Zboru <zboru99@gmail.com>
2021-08-30 13:25:08 +0200

    zabezpieczenie ścieżek logowaniem

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import firebase from 'firebase';
 import Dashboard from '../views/Dashboard.vue';
 import CreateNewGame from '../views/CreateNewGame.vue';
 import Browse from '../views/Browse.vue';
+import Auth from '../views/Auth.vue';
 
 Vue.use(VueRouter);
 
@@ -11,16 +13,33 @@ const routes = [
     path: '/',
     name: 'Dashboard',
     component: Dashboard,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/browse',
     name: 'Browse',
     component: Browse,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/create',
     name: 'CreateNewGame',
     component: CreateNewGame,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Auth,
+    meta: {
+      plainLayout: true,
+    },
   },
 ];
 
@@ -28,6 +47,15 @@ const router = new VueRouter({
   mode: 'hash',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  if (requiresAuth && !await firebase.getCurrentUser()) {
+    next('Login');
+  } else {
+    next();
+  }
 });
 
 export default router;
