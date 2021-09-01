96b8de2115540fd838ae55444de89efea736bd64
Zboru <zboru99@gmail.com>
2021-09-01 14:53:30 +0200

    dodanie avataru użytkownika do menu profilu

--- src/components/Layout/ProfileMenu.vue
+++ src/components/Layout/ProfileMenu.vue
@@ -2,11 +2,11 @@
   <div class="relative inline-block text-left">
     <div>
       <button type="button" @focus="state = true" @focusout="state =false" tabindex="-1"
-              class="bg-gray-800 ml-2 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              class="ml-2 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
               id="user-menu-button" aria-expanded="false" aria-haspopup="true">
         <span class="sr-only">Open user menu</span>
-        <img class="h-8 w-8 rounded-full"
-             src="https://avatars.dicebear.com/api/micah/21p.svg"
+        <img class="h-8 w-8 border rounded-full"
+             :src="user.avatar.url"
              alt="">
       </button>
     </div>
@@ -15,10 +15,10 @@
          role="menu" :class="menuClasses" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
       <p class="px-4 py-2 text-sm font-normal">
         Signed in as:
-        <t-skeleton width="w-3/4" v-if="!userEmail"></t-skeleton>
-        <span class="font-medium" v-else><br>{{ userEmail }}</span>
-        <t-skeleton v-if="!userNick" width="w-1/2"></t-skeleton>
-        <span v-else class="text-xs text-gray-400 italic font-medium"><br>{{ userNick }}</span>
+        <t-skeleton width="w-3/4" v-if="!user.email"></t-skeleton>
+        <span class="font-medium" v-else><br>{{ user.email }}</span>
+        <t-skeleton v-if="!user.nick" width="w-1/2"></t-skeleton>
+        <span v-else class="text-xs text-gray-400 italic font-medium"><br>{{ user.nick }}</span>
       </p>
       <hr>
       <div>
@@ -58,11 +58,8 @@ export default {
         'transform opacity-100 scale-100': this.state,
       };
     },
-    userEmail() {
-      return this.$store.getters.getUser.email;
-    },
-    userNick() {
-      return this.$store.getters.getUser.nick;
+    user() {
+      return { ...this.$store.getters.getUser };
     },
   },
   methods: {
