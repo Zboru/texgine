ccded81033def6177de6a09084a21879a4739e2a
Zboru <zboru99@gmail.com>
2021-09-15 13:09:25 +0200

    naprawienie podświetlenia ścieżki nawigacji

--- src/components/Layout/Navigation.vue
+++ src/components/Layout/Navigation.vue
@@ -21,11 +21,11 @@
           </div>
           <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div class="flex-shrink-0 flex items-center">
-              <p class="navbar-brand">texGine</p>
+              <p class="navbar-brand">tex<span class="text-green-500">Gine</span></p>
             </div>
             <div class="hidden sm:block sm:ml-6">
               <div class="flex space-x-4 navbar-links">
-                <router-link to="/" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Dashboard</router-link>
+                <router-link to="/" class="text-gray-500 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Dashboard</router-link>
                 <router-link to="/browse"
                              class="text-gray-500 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Browse</router-link>
                 <router-link to="/my-games"
