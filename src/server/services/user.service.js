86d49908673a3e857b59b9143f1ce3fb5fdde641
Zboru <zboru99@gmail.com>
2021-09-23 10:19:19 +0200

    zmiana autora w grach w momencie zmiany nickname

--- src/server/services/user.service.js
+++ src/server/services/user.service.js
@@ -1,5 +1,9 @@
-const { app, db } = require('../firebase');
-const {doc, getDoc, updateDoc} = require('firebase/firestore');
+const { db } = require('../firebase');
+const {
+  doc,
+  getDoc,
+  updateDoc
+} = require('firebase/firestore');
 
 const getGames = async function (userId) {
   try {
@@ -16,12 +20,27 @@ const setUser = async function (userId, data) {
   try {
     const userRef = await doc(db, 'users', userId);
     const userSnap = await getDoc(userRef);
+    // Change author nick in games
+    let userData = userSnap.data();
+    const games = userData.games;
+    if (games && Object.keys(games).length > 0) {
+      for (let game in games) {
+        if (games.hasOwnProperty(game)) {
+          games[game].author = data.nick;
+        }
+      }
+    }
+    data.games = games;
+
     await updateDoc(userRef, data);
-    return userSnap.data();
+    return userData;
   } catch (err) {
     console.log(err);
     return err;
   }
 };
 
-module.exports = { setUser, getGames };
+module.exports = {
+  setUser,
+  getGames
+};
