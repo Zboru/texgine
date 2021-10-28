b84fb38bc844647cae98b4787f6a3d7d120c7b9c
Zboru <zboru99@gmail.com>
2021-10-28 11:12:43 +0200

    blokada inputów hasła jeśli używany jest provider logowania

--- src/components/Profile/PasswordSection.vue
+++ src/components/Profile/PasswordSection.vue
@@ -5,22 +5,22 @@
         <div class="px-4 sm:px-0">
           <h3 class="text-lg font-medium leading-6 text-gray-900">Authentication settings</h3>
           <p class="mt-1 text-sm text-gray-600">
-            Set your new strong password here.
+            Set your new strong password here. You can't change password if you're using provider to log in.
           </p>
         </div>
       </div>
       <div class="mt-5 md:mt-0 md:col-span-2">
         <div class="shadow sm:rounded-md">
           <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
-            <t-text-field placeholder="*********" v-model="password" label="New password"></t-text-field>
+            <t-text-field :disabled="usingProvider" placeholder="*********" v-model="password" label="New password"></t-text-field>
             <div>
               <div class="mt-1">
-                <t-text-field label="Confirm password" v-model="confirmPassword" placeholder="*********"></t-text-field>
+                <t-text-field :disabled="usingProvider" label="Confirm password" v-model="confirmPassword" placeholder="*********"></t-text-field>
               </div>
             </div>
           </div>
           <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-            <t-button :loading="loading.save" :disabled="loading.save" @click="save" variant="success"
+            <t-button :loading="loading.save" :disabled="loading.save || usingProvider" @click="save" variant="success"
                       class="inline-flex">
               Save
             </t-button>
@@ -66,6 +66,9 @@ export default {
       return (Math.random() + 1).toString(36)
         .substring(3);
     },
+    usingProvider() {
+      return this.$store.getters.getUser.provider !== null;
+    }
   },
   methods: {
     save() {
