88e6adba2ad4307df8a1c171616b4b76cc92530b
Zboru <zboru99@gmail.com>
2021-10-28 14:59:04 +0200

    implementacja oceniania komentarzy

--- src/components/Browse/GameComment.vue
+++ src/components/Browse/GameComment.vue
@@ -1,33 +1,35 @@
 <template>
-  <div class="border-2 p-2 mt-1 rounded flex flex-col">
-    <div class="flex">
-      <img :src="comment.authorAvatar" class="w-12 h-12 border mr-3 select-none" alt="">
-      <div class="flex flex-col flex-grow text-sm">
-        <p class="text-gray-400 italic font-medium select-none flex justify-between">
-          <span>{{ comment.author }} wrote:</span>
-          <span>{{ getDate(comment.created_at) }}</span>
-        </p>
-        <p>{{ comment.text }}</p>
-      </div>
-    </div>
-    <div class="flex justify-between">
-      <div class="flex justify-around mt-1 w-12">
-        <t-icon @click="vote('upvote')" class="cursor-pointer hover:text-green-500" icon="bx:bx-upvote"></t-icon>
-        <t-icon @click="vote('downvote')" class="cursor-pointer hover:text-red-500" icon="bx:bx-downvote"></t-icon>
-      </div>
-      <div class="flex items-center">
+	<div class="border-2 p-2 mt-1 rounded flex flex-col">
+		<div class="flex">
+			<img :src="comment.authorAvatar" class="w-12 h-12 border mr-3 select-none" alt="">
+			<div class="flex flex-col flex-grow text-sm">
+				<p class="text-gray-400 italic font-medium select-none flex justify-between">
+					<span>{{ comment.author }} wrote:</span>
+					<span>{{ getDate(comment.created_at) }}</span>
+				</p>
+				<p>{{ comment.text }}</p>
+			</div>
+		</div>
+		<div class="flex justify-between">
+			<div class="flex justify-around mt-1 w-12">
+				<t-icon @click="vote('1')" :class="{'text-green-500': voted(1)}" class="cursor-pointer hover:text-green-500"
+						icon="bx:bx-upvote"></t-icon>
+				<t-icon @click="vote('-1')" :class="{'text-red-500': voted(-1)}" class="cursor-pointer hover:text-red-500"
+						icon="bx:bx-downvote"></t-icon>
+			</div>
+			<div class="flex items-center">
         <span class="flex items-center">
           <t-icon class="mx-1 text-green-500" icon="bx:bx-upvote"></t-icon>
-          0
+          {{upvotes}}
         </span>
-        <span class="flex items-center">
+				<span class="flex items-center">
           <t-icon class="mx-1 text-red-500" icon="bx:bx-downvote"></t-icon>
-          0
+          {{downvotes}}
         </span>
-        <t-icon class="cursor-pointer ml-2" icon="heroicons-outline:dots-vertical"></t-icon>
-      </div>
-    </div>
-  </div>
+				<t-icon class="cursor-pointer ml-2" icon="heroicons-outline:dots-vertical"></t-icon>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -35,36 +37,47 @@ import TIcon from '../General/TIcon.vue';
 import httpManager from '../../utils/httpManager';
 
 export default {
-  name: 'GameComment',
-  components: { TIcon },
-  props: {
-    comment: {
-      type: Object,
-    }
-  },
-  computed: {
-    randomAvatar() {
-      const seed = Math.floor(Math.random() * 16);
-      return `https://avatars.dicebear.com/api/micah/${seed}.svg`;
-    },
-    listGame() {
-      return this.$store.getters.getListGame;
-    }
-  },
-  methods: {
-    getDate(date) {
-      if (typeof date === 'object') {
-        const unix = parseInt(date.seconds + '000');
-        return new Date(unix).toLocaleString();
-      } else {
-        return new Date(date).toLocaleString();
-      }
-    },
-    vote(type) {
-      httpManager.post(`${import.meta.env.VITE_API_URL}/games/${this.listGame.id}/comment/${this.comment.id}/vote`, {
-        type: type
-      });
-    }
-  }
+	name: 'GameComment',
+	components: { TIcon },
+	props: {
+		comment: {
+			type: Object,
+		}
+	},
+	computed: {
+		randomAvatar() {
+			const seed = Math.floor(Math.random() * 16);
+			return `https://avatars.dicebear.com/api/micah/${seed}.svg`;
+		},
+		listGame() {
+			return this.$store.getters.getListGame;
+		},
+		upvotes() {
+			return Object.values(this.comment.votes).filter(vote => vote === 1).length;
+		},
+		downvotes() {
+			return Object.values(this.comment.votes).filter(vote => vote !== 1).length;
+		}
+	},
+	methods: {
+		getDate(date) {
+			if (typeof date === 'object') {
+				const unix = parseInt(date.seconds + '000');
+				return new Date(unix).toLocaleString();
+			} else {
+				return new Date(date).toLocaleString();
+			}
+		},
+		vote(type) {
+			httpManager.post(`${import.meta.env.VITE_API_URL}/games/${this.listGame.id}/comment/${this.comment.id}/vote`, {
+				type: type
+			}).then(response => {
+				this.comment.votes = response.data.votes;
+			});
+		},
+		voted(type){
+			return this.comment.votes[this.$store.getters.getUser.uid] === type;
+		}
+	}
 };
 </script>
--- src/server/services/games.service.js
+++ src/server/services/games.service.js
@@ -1,189 +1,194 @@
 const {
-  db
+	db
 } = require('../firebase');
 const {
-  doc,
-  getDoc,
-  setDoc,
-  deleteDoc
+	doc,
+	getDoc,
+	setDoc,
+	deleteDoc
 } = require('firebase/firestore');
 
 function generateId() {
-  return Math.random()
-    .toString(36)
-    .replace(/[^a-z0-9]+/g, '');
+	return Math.random()
+		.toString(36)
+		.replace(/[^a-z0-9]+/g, '');
 }
 
 const createGame = async function (userId) {
-  try {
-    const userRef = await doc(db, 'users', userId);
-    const userSnap = await getDoc(userRef);
-    const userData = userSnap.data();
+	try {
+		const userRef = await doc(db, 'users', userId);
+		const userSnap = await getDoc(userRef);
+		const userData = userSnap.data();
 
-    const gameId = generateId();
-    const game = {
-      id: gameId,
-      author: userData.nick,
-      author_uid: userData.uid,
-      description: null,
-      steps: {},
-      canvas: {},
-      title: 'Untitled',
-      play_count: 0,
-      favorite_count: 0,
-      rating: 0,
-      comments: {},
-      public: false,
-      created_at: new Date(),
-      updated_at: new Date(),
-    };
-    // Save game to games collection
-    const gameRef = await doc(db, 'games', gameId);
-    await setDoc(gameRef, game);
+		const gameId = generateId();
+		const game = {
+			id: gameId,
+			author: userData.nick,
+			author_uid: userData.uid,
+			description: null,
+			steps: {},
+			canvas: {},
+			title: 'Untitled',
+			play_count: 0,
+			favorite_count: 0,
+			rating: 0,
+			comments: {},
+			public: false,
+			created_at: new Date(),
+			updated_at: new Date(),
+		};
+		// Save game to games collection
+		const gameRef = await doc(db, 'games', gameId);
+		await setDoc(gameRef, game);
 
-    // Add game data to user's games collection
-    userData.games.push(gameRef);
-    await setDoc(userRef, userData);
+		// Add game data to user's games collection
+		userData.games.push(gameRef);
+		await setDoc(userRef, userData);
 
-    return game;
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
+		return game;
+	} catch (err) {
+		console.error(err);
+		return err;
+	}
 };
 
 const cloneGame = async function (userId, gameId) {
-  try {
-    const userRef = await doc(db, 'users', userId);
-    const userSnap = await getDoc(userRef);
-    const userData = userSnap.data();
+	try {
+		const userRef = await doc(db, 'users', userId);
+		const userSnap = await getDoc(userRef);
+		const userData = userSnap.data();
 
-    const gameDoc = await getDoc(doc(db, 'games', gameId));
-    const game = gameDoc.data();
+		const gameDoc = await getDoc(doc(db, 'games', gameId));
+		const game = gameDoc.data();
 
-    // Generate new game id
-    const newGameId = generateId();
+		// Generate new game id
+		const newGameId = generateId();
 
-    // Create new game object and override author and its id
-    const newGame = {
-      ...game,
-      author: userData.nick,
-      author_uid: userData.uid,
-      comments: {},
-      play_count: 0,
-      favorite_count: 0,
-      rating: 0,
-      public: false,
-      title: `${game.title} - Clone`,
-      id: newGameId,
-    };
-    // Save game to games collection
-    const gameRef = await doc(db, 'games', newGameId);
-    await setDoc(gameRef, newGame);
+		// Create new game object and override author and its id
+		const newGame = {
+			...game,
+			author: userData.nick,
+			author_uid: userData.uid,
+			comments: {},
+			play_count: 0,
+			favorite_count: 0,
+			rating: 0,
+			public: false,
+			title: `${game.title} - Clone`,
+			id: newGameId,
+		};
+		// Save game to games collection
+		const gameRef = await doc(db, 'games', newGameId);
+		await setDoc(gameRef, newGame);
 
-    // Add game to user games collection
-    userData.games.push(gameRef);
-    await setDoc(userRef, userData);
+		// Add game to user games collection
+		userData.games.push(gameRef);
+		await setDoc(userRef, userData);
 
-    return newGame;
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
+		return newGame;
+	} catch (err) {
+		console.error(err);
+		return err;
+	}
 };
 
 const deleteGame = async function (userId, gameId) {
-  try {
-    // Delete game reference from array
-    const userRef = await doc(db, 'users', userId);
-    const userSnap = await getDoc(userRef);
-    const userData = userSnap.data();
+	try {
+		// Delete game reference from array
+		const userRef = await doc(db, 'users', userId);
+		const userSnap = await getDoc(userRef);
+		const userData = userSnap.data();
 
-    const games = userData.games;
-    const gameIndex = games.findIndex(game => game.id === gameId);
-    games.splice(gameIndex, 1);
-    await setDoc(userRef, userData);
+		const games = userData.games;
+		const gameIndex = games.findIndex(game => game.id === gameId);
+		games.splice(gameIndex, 1);
+		await setDoc(userRef, userData);
 
-    // Remove game from games collection
-    await deleteDoc(doc(db, 'games', gameId));
+		// Remove game from games collection
+		await deleteDoc(doc(db, 'games', gameId));
 
-    return userData;
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
+		return userData;
+	} catch (err) {
+		console.error(err);
+		return err;
+	}
 };
 
 const saveGame = async function (userId, gameId, data) {
-  try {
-    const gameRef = await doc(db, 'games', gameId);
-    await setDoc(gameRef, data);
-    return data;
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
+	try {
+		const gameRef = await doc(db, 'games', gameId);
+		await setDoc(gameRef, data);
+		return data;
+	} catch (err) {
+		console.error(err);
+		return err;
+	}
 };
 
 const addComment = async function (userId, gameId, data) {
-  try {
-    const gameRef = await doc(db, 'games', gameId);
-    const gameSnap = await getDoc(gameRef);
-    const gameData = gameSnap.data();
+	try {
+		const gameRef = await doc(db, 'games', gameId);
+		const gameSnap = await getDoc(gameRef);
+		const gameData = gameSnap.data();
 
-    const userRef = await doc(db, 'users', userId);
-    const userSnap = await getDoc(userRef);
-    const userData = userSnap.data();
+		const userRef = await doc(db, 'users', userId);
+		const userSnap = await getDoc(userRef);
+		const userData = userSnap.data();
 
-    // Create comment object in case it does not exist
-    if (typeof gameData.comments === 'undefined') {
-      gameData.comments = {};
-    }
+		// Create comment object in case it does not exist
+		if (typeof gameData.comments === 'undefined') {
+			gameData.comments = {};
+		}
 
-    const commentId = generateId();
-    const comment = {
-      id: commentId,
-      author: userData.nick,
-      author_uid: userData.uid,
-      authorAvatar: userData.avatar.url,
-      text: data.comment,
-      created_at: new Date(),
-      votes: {}
-    };
+		const commentId = generateId();
+		const comment = {
+			id: commentId,
+			author: userData.nick,
+			author_uid: userData.uid,
+			authorAvatar: userData.avatar.url,
+			text: data.comment,
+			created_at: new Date(),
+			votes: {}
+		};
 
-    gameData.comments[commentId] = comment;
-    await setDoc(gameRef, gameData);
+		gameData.comments[commentId] = comment;
+		await setDoc(gameRef, gameData);
 
-    return comment;
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
+		return comment;
+	} catch (err) {
+		console.error(err);
+		return err;
+	}
 };
 
 const vote = async function (userId, gameId, commentId, type) {
-  try {
-    const gameRef = await doc(db, 'games', gameId);
-    const gameSnap = await getDoc(gameRef);
-    const gameData = gameSnap.data();
+	try {
+		const gameRef = await doc(db, 'games', gameId);
+		const gameSnap = await getDoc(gameRef);
+		const gameData = gameSnap.data();
 
-    const comment = gameData.comments[commentId];
+		const comment = gameData.comments[commentId];
 
-    comment.votes[userId] = type === 'upvote' ? 1 : -1;
+		// If vote already exists, delete it
+		if (comment.votes[userId] && comment.votes[userId] === parseInt(type)) {
+			delete comment.votes[userId];
+		} else {
+			comment.votes[userId] = parseInt(type);
+		}
 
-    await setDoc(gameRef, gameData);
-    return comment;
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
+		await setDoc(gameRef, gameData);
+		return comment;
+	} catch (err) {
+		console.error(err);
+		return err;
+	}
 };
 
 module.exports = {
-  createGame,
-  cloneGame,
-  deleteGame,
-  saveGame,
-  addComment,
-  vote
+	createGame,
+	cloneGame,
+	deleteGame,
+	saveGame,
+	addComment,
+	vote
 };
